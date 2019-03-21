106b545e752aea3a745b71cb48524b79f6c5dd2f
Waga Odongo <wagaodongo@gmail.com>
2019-03-21 21:56:31 +0300

    create context
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

---
+++ src/context.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TodosContext = React.createContext({
+	todos: [
+		{ id: 1, text: "Eat Breakfast", complete: false },
+		{ id: 2, text: "Finish laundry", complete: false },
+		{ id: 3, text: "Do Project", complete: true }
+	]
+});
