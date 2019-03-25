28d226f37d0f3a45a45245a9c299bd4d9a396099
Waga Odongo <wagaodongo@gmail.com>
2019-03-25 15:24:27 +0300

    strike through todos
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import TodosContext from "../context";
 
 export default function TodoList() {
-	const { state } = useContext(TodosContext);
+	const { state, dispatch } = useContext(TodosContext);
 	const title =
 		state.todos.length > 0 ? `${state.todos.length} Todos` : "Nothing To Do";
 	return (
@@ -14,7 +14,15 @@ export default function TodoList() {
 						key={todo.id}
 						className=" flex items-center bg-orange-dark border-black border-dashed border-2"
 					>
-						<span className="flex-1 ml-12 cursos-pointer">{todo.text} </span>
+						<span
+							onDoubleClick={() =>
+								dispatch({ type: "TOGGLE_TODO", payload: todo })
+							}
+							className={`flex-1 ml-12 cursos-pointer ${todo.complete &&
+								"line-through text-grey-darkest "}`}
+						>
+							{todo.text}{" "}
+						</span>
 						<button>
 							<img
 								src="https://icon.now.sh/edit/0050c5"
